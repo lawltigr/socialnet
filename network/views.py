5c6888984cb649bad69abc9015ec57d56d4b5762
lawltigr <vasilisatikhonova110@gmail.com>
2026-01-19 21:56:19 -0800

    continued workinggg

--- network/views.py
+++ network/views.py
@@ -29,6 +29,9 @@ def feed(request):
                 return redirect('feed')
             
         elif 'comment_submit' in request.POST:
+            parent_id = request.POST.get('parent_id')
+            parent_comment = Comment.objects.get(id=parent_id)
+            reply_to = request.POST.get('reply_to')
             post_id = request.POST.get('post_id')
             post = Post.objects.get(id=post_id)
             comment_form = CommentForm(request.POST)
@@ -36,9 +39,12 @@ def feed(request):
                 comment = comment_form.save(commit=False)
                 comment.post = post
                 comment.author = request.user
+                if reply_to:
+                    parent_comment = Comment.objects.get(id=reply_to)
+                    comment.parent = parent_comment
                 comment.save()
                 Notification.objects.create(
-                    to_user = post.author,
+                    to_user = parent_comment.author,
                     from_user = request.user, 
                     notification_type = 'comment',
                     post = post,
