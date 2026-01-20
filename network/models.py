464e05f3d72aab04fc54ee29dd183bbcffb96264
lawltigr <vasilisatikhonova110@gmail.com>
2026-01-19 21:43:07 -0800

    started comment reply

--- network/models.py
+++ network/models.py
@@ -33,6 +33,7 @@ class Comment(models.Model):
     author=models.ForeignKey(User, on_delete=models.CASCADE)
     content=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
 
     def __str__(self):
         return f'{self.author.username} on {self.post.id}'
